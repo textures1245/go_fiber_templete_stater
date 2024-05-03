e17a6f97052101fb68bd06062106d8a36b418720
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-03 11:01:55 +0700

    update(user-repo-query): update user repository query names and add missing queries

--- internal/user/repository/repository_query/query.go
+++ internal/user/repository/repository_query/query.go
@@ -4,5 +4,14 @@ import _ "embed"
 
 var (
 	//go:embed user/get_users.sql
-	InsertUsers string
+	GetUsers string
+
+	//go:embed user/find_user_by_id.sql
+	FindUserById string
+
+	//go:embed user/find_user_by_user_credential.sql
+	FindUserByUsernameAndPassword string
+
+	//go:embed user/insert.sql
+	InsertUser string
 )
