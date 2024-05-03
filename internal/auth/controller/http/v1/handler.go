a63bd340e45febe874a294bd31821cb759479079
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-03 15:04:26 +0700

    feat(auth-controller): Add HTTP handler for authentication endpoints

---
+++ internal/auth/controller/http/v1/handler.go
@@ -0,0 +1,86 @@
+package v1
+
+import (
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/textures1245/go-template/internal/auth"
+	"github.com/textures1245/go-template/internal/auth/entities"
+	_userEntities "github.com/textures1245/go-template/internal/user/entities"
+)
+
+type authConn struct {
+	AuthUse auth.AuthUsecase
+}
+
+func NewAuthHandler(authUse auth.AuthUsecase) *authConn {
+	return &authConn{
+		AuthUse: authUse,
+	}
+}
+
+func (a *authConn) Login(c *fiber.Ctx) error {
+	req := new(entities.UsersCredentials)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	res, status, err := a.AuthUse.Login(ctx, req)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      http.StatusText(status),
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(status).JSON(fiber.Map{
+		"status":      http.StatusText(status),
+		"status_code": status,
+		"message":     "",
+		"result":      res,
+	})
+}
+
+func (a *authConn) Register(c *fiber.Ctx) error {
+	req := new(_userEntities.UserCreatedReq)
+	if err := c.BodyParser(req); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"message": err.Error(),
+		})
+	}
+
+	var (
+		ctx, cancel = context.WithTimeout(c.Context(), time.Duration(30*time.Second))
+	)
+
+	defer cancel()
+
+	res, status, err := a.AuthUse.Register(ctx, req)
+	if err != nil {
+		return c.Status(status).JSON(fiber.Map{
+			"status":      http.StatusText(status),
+			"status_code": status,
+			"message":     err.Error(),
+			"result":      nil,
+		})
+	}
+
+	return c.Status(status).JSON(fiber.Map{
+		"status":      http.StatusText(status),
+		"status_code": status,
+		"message":     "",
+		"result":      res,
+	})
+}
