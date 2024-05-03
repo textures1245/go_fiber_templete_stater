e67adba4f25cc9a0548745b1cf725a2ce9cd2901
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-03 17:56:46 +0700

    update(user-dtos): Add IdCard field to UserDetailRespond struct

--- internal/user/dtos/user_detail.go
+++ internal/user/dtos/user_detail.go
@@ -5,6 +5,7 @@ type UserDetailRespond struct {
 	Name        string `json:"name"`
 	Email       string `json:"email" db:"email"`
 	PhoneNumber string `json:"phone_number" db:"phone_number"`
+	IdCard      string `json:"id_card"`
 	UpdatedAt   string `json:"updated_at"`
 	CreatedAt   string `json:"created_at"`
 }
