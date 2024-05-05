9b85181fdcf7343c9d7ffbbfe4676463c3366e9f
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-06 00:18:35 +0700

    update(utils): Add Contains function to Array struct in array.go

--- pkg/utils/array.go
+++ pkg/utils/array.go
@@ -40,3 +40,12 @@ func (l *Array) Map(fn func(k int, v interface{})) {
 		}
 	})
 }
+
+func Contains(slice []interface{}, item interface{}) bool {
+	for _, a := range slice {
+		if a == item {
+			return true
+		}
+	}
+	return false
+}
