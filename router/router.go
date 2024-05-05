121c76da58d7d3972997de6a9e9bcb140a36c92a
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-05 19:27:28 +0700

    update(route): Refactor router setup in router.go to use separate packages for auth and user controllers

--- router/router.go
+++ router/router.go
@@ -9,17 +9,11 @@ import (
 	"github.com/textures1245/go-template/repository"
 	"github.com/textures1245/go-template/service"
 
-	_userConn "github.com/textures1245/go-template/internal/user/controller/http/v1"
-	_userRepo "github.com/textures1245/go-template/internal/user/repository"
-	_userUsecase "github.com/textures1245/go-template/internal/user/usecase"
-
-	_authConn "github.com/textures1245/go-template/internal/auth/controller/http/v1"
-	_authRepo "github.com/textures1245/go-template/internal/auth/repository"
-	_authUsecase "github.com/textures1245/go-template/internal/auth/usecase"
+	authV1 "github.com/textures1245/go-template/internal/auth/controller/http/v1"
+	userV1 "github.com/textures1245/go-template/internal/user/controller/http/v1"
 
 	// "github.com/textures1245/go-template/config"
 	// "github.com/textures1245/go-template/pkg/datasource"
-	"github.com/textures1245/go-template/pkg/middleware"
 
 	"github.com/gofiber/fiber/v2"
 	log "github.com/sirupsen/logrus"
@@ -47,42 +41,17 @@ func SetupRoutes(app *fiber.App) {
 
 	db := repository.GetDb()
 
-	userRepo := _userRepo.NewUserRepository(db)
-	userService := _userUsecase.NewUserUsecase(userRepo)
-	userConn := _userConn.NewUserController(userService)
-
 	api := app.Group("/", func(c *fiber.Ctx) error {
 		if !strings.Contains(c.Request().URI().String(), "/ping") {
 			log.Infof("all : %v", c.Request().URI().String())
 		}
 		return c.Next()
 	})
-
-	userR := app.Group("/api/user", func(c *fiber.Ctx) error {
-		if !strings.Contains(c.Request().URI().String(), "/ping") {
-			log.Infof("all : %v", c.Request().URI().String())
-		}
-		return c.Next()
-	})
-
-	authR := app.Group("/api/auth", func(c *fiber.Ctx) error {
-		if !strings.Contains(c.Request().URI().String(), "/ping") {
-			log.Infof("all : %v", c.Request().URI().String())
-		}
-		return c.Next()
-	})
-
-	authRepo := _authRepo.NewAuthRepository(db)
-	authService := _authUsecase.NewAuthService(authRepo, userRepo)
-	authConn := _authConn.NewAuthHandler(authService)
-
 	api.Get("/api/something", controller.SampleControllerFunction)
 	api.Get("/api/ping", controller.Ping)
 
-	userR.Get("/get-users", middleware.JwtAuthentication(), userConn.FetchUsers)
-
-	authR.Post("/customer/login", authConn.Login)
-	authR.Post("/customer/register", authConn.Register)
+	userV1.UseUserRoute(db, app)
+	authV1.UseAuthRoute(db, app)
 
 	callback := app.Group("/callback", func(c *fiber.Ctx) error {
 		log.Infof("callback : %v", c.Request().URI().String())
