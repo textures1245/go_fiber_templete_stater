d9a4a10efbb1d32f1b558d1052858b051262fcd6
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-07 11:53:51 +0700

    test(auth-usecase): Add unit tests for Register function in AuthUsecase

--- internal/auth/usecase/usecase_test.go
+++ internal/auth/usecase/usecase_test.go
@@ -7,10 +7,12 @@ import (
 
 	"testing"
 
+	"github.com/gofiber/fiber/v2/log"
 	"github.com/stretchr/testify/require"
 	"github.com/textures1245/go-template/internal/auth/dtos"
 	"github.com/textures1245/go-template/internal/auth/entities"
 	mock_auth "github.com/textures1245/go-template/internal/auth/mock"
+	userEntities "github.com/textures1245/go-template/internal/user/entities"
 	mock_user "github.com/textures1245/go-template/internal/user/mock"
 	"github.com/textures1245/go-template/pkg/apperror"
 	"go.uber.org/mock/gomock"
@@ -68,22 +70,13 @@ func Test_usecase_Login(t *testing.T) {
 		Password: string(hashPassword),
 	}
 
-	// userCreated := &userEntities.UserCreatedReq{
-	// 	Name:        "test",
-	// 	Username:    username,
-	// 	Password:    password,
-	// 	Email:       "test@gmail.com",
-	// 	PhoneNumber: "08123456789",
-	// 	IdCard:      "1234567890",
-	// }
-
 	t.Run("login_positive_case", func(t *testing.T) {
 		// mock.
 		// 	ExpectExec(repository_query.InsertUser).
-		// 	WithArgs(userCreated.Name, userCreated.Username, userCreated.Password, userCreated.Email, userCreated.PhoneNumber, userCreated.IdCard).
+		// 	WithArgs(userCred.Name, userCred.Username, userCred.Password, userCred.Email, userCred.PhoneNumber, userCred.IdCard).
 		// 	WillReturnResult(sqlmock.NewResult(userId, 1))
 
-		// userID, error := userRepo.CreateUser(context.Background(), userCreated)
+		// userID, error := userRepo.CreateUser(context.Background(), userCred)
 		// assert.NoError(t, error)
 		// assert.NotEmpty(t, userID)
 
@@ -107,10 +100,10 @@ func Test_usecase_Login(t *testing.T) {
 	t.Run("login_negative_password_invalid", func(t *testing.T) {
 		// mock.
 		// 	ExpectExec(repository_query.InsertUser).
-		// 	WithArgs(userCreated.Name, userCreated.Username, userCreated.Password, userCreated.Email, userCreated.PhoneNumber, userCreated.IdCard).
+		// 	WithArgs(userCred.Name, userCred.Username, userCred.Password, userCred.Email, userCred.PhoneNumber, userCred.IdCard).
 		// 	WillReturnResult(sqlmock.NewResult(userId, 1))
 
-		// userID, error := userRepo.CreateUser(context.Background(), userCreated)
+		// userID, error := userRepo.CreateUser(context.Background(), userCred)
 		// assert.NoError(t, error)
 		// assert.NotEmpty(t, userID)
 
@@ -142,10 +135,10 @@ func Test_usecase_Login(t *testing.T) {
 	t.Run("login_negative_failed_query_username", func(t *testing.T) {
 		// mock.
 		// 	ExpectExec(repository_query.InsertUser).
-		// 	WithArgs(userCreated.Name, userCreated.Username, userCreated.Password, userCreated.Email, userCreated.PhoneNumber, userCreated.IdCard).
+		// 	WithArgs(userCred.Name, userCred.Username, userCred.Password, userCred.Email, userCred.PhoneNumber, userCred.IdCard).
 		// 	WillReturnResult(sqlmock.NewResult(userId, 1))
 
-		// userID, error := userRepo.CreateUser(context.Background(), userCreated)
+		// userID, error := userRepo.CreateUser(context.Background(), userCred)
 		// assert.NoError(t, error)
 		// assert.NotEmpty(t, userID)
 
@@ -158,9 +151,75 @@ func Test_usecase_Login(t *testing.T) {
 			RawError: sql.ErrNoRows,
 		})
 		userToken, status, err := aMockUse.Login(ctx, userCred)
+		log.Debug(err)
 		require.EqualError(t, err.RawError, sql.ErrNoRows.Error())
 		require.Equal(t, status, http.StatusNotFound)
 		require.Equal(t, userToken, &dtos.UserTokenRes{})
 	})
 
+}
+
+func Test_usecase_Register(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	var (
+		username       = "username_test"
+		password       = "password_test"
+		userId   int64 = 1
+	)
+
+	ctx := context.Background()
+	userMockRepo := mock_user.NewMockUserRepository(ctrl)
+	aMockUse := mock_auth.NewMockAuthUsecase(ctrl)
+
+	userCreated := &userEntities.UserCreatedReq{
+		Name:        "test",
+		Username:    username,
+		Password:    password,
+		Email:       "test@gmail.com",
+		PhoneNumber: "08123456789",
+		IdCard:      "1234567890",
+	}
+
+	t.Run("register_positive_case", func(t *testing.T) {
+		userMockRepo.EXPECT().CreateUser(ctx, userCreated).Return(&userId, nil)
+		usrId, err := userMockRepo.CreateUser(ctx, userCreated)
+		require.NoError(t, err)
+		require.NotNil(t, usrId)
+
+		aMockUse.EXPECT().Register(ctx, userCreated).Return(&dtos.UsersRegisteredRes{
+			AccessToken: "Authorization",
+		}, http.StatusOK, nil)
+		userToken, status, cErr := aMockUse.Register(ctx, userCreated)
+
+		var rawError error
+		if cErr != nil {
+			rawError = cErr.RawError
+		}
+		require.NoError(t, rawError)
+		require.Equal(t, status, http.StatusOK)
+		require.NotNil(t, userToken)
+	})
+
+	t.Run("register_negative_failed_unique_username_created", func(t *testing.T) {
+
+		aMockUse.EXPECT().Register(ctx, userCreated).Return(&dtos.UsersRegisteredRes{}, 404, &apperror.CErr{
+			RawError: sql.ErrNoRows,
+		})
+		userToken, status, cErr2 := aMockUse.Register(ctx, userCreated)
+		var rawError2 error
+		if cErr2 != nil {
+			rawError2 = cErr2.RawError
+		}
+		require.EqualError(t, rawError2, sql.ErrNoRows.Error())
+		require.Equal(t, status, http.StatusNotFound)
+		require.Equal(t, userToken, &dtos.UsersRegisteredRes{})
+
+		userMockRepo.EXPECT().CreateUser(ctx, userCreated).Return(nil, sql.ErrNoRows)
+		usrId, err := userMockRepo.CreateUser(ctx, userCreated)
+		require.Nil(t, usrId)
+		require.Error(t, err)
+
+	})
 }
