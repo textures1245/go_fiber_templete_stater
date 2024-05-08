248b9f8c7a96c9752c2d4842cdc11e49d2d0d9c3
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-08 21:45:46 +0700

    refactor(file-entities): Refactor image encoding and file saving logic in file package

--- internal/file/entities/file.go
+++ internal/file/entities/file.go
@@ -2,7 +2,9 @@ package entities
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/base64"
+	"encoding/hex"
 	"errors"
 	"fmt"
 	"image"
@@ -10,7 +12,6 @@ import (
 	"image/png"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/gofiber/fiber/v2/log"
 )
@@ -31,37 +32,47 @@ func (f *File) Base64toPng() (string, error) {
 	}
 
 	fileData := base64.StdEncoding.EncodeToString(f.FileData)
-	log.Info("File data", fileData)
+	hasher := sha256.New()
+	hasher.Write([]byte(fileData))
+	hash := hex.EncodeToString(hasher.Sum(nil))
 
-	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(fileData))
-	m, formatString, err := image.Decode(reader)
+	pngFilename := "public/image/" + hash + ".png"
+
+	if _, err := os.Stat(pngFilename); os.IsNotExist(err) {
+		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(fileData))
+		m, _, err := image.Decode(reader)
+		if err != nil {
+			log.Fatal(err)
+		}
+		// bounds := m.Bounds()
+		// fmt.Println(bounds, formatString)
+
+		osFile, errOnOpenFIle := os.OpenFile(pngFilename, os.O_WRONLY|os.O_CREATE, 0777)
+		if errOnOpenFIle != nil {
+			log.Fatal(errOnOpenFIle)
+		}
+		err = png.Encode(osFile, m)
+		if err != nil {
+			log.Fatal(err)
+		}
+		buffer := new(bytes.Buffer)
+		errWhileEncoding := png.Encode(buffer, m) // img is your image.Image
+		if errWhileEncoding != nil {
+			log.Fatal(errWhileEncoding)
+		}
+		srcImg := fmt.Sprintf("data:image/png;base64,%s", base64.StdEncoding.EncodeToString(buffer.Bytes()))
+		log.Info("Create new PNG file name: ", pngFilename, "as the output")
+
+		return srcImg, nil
+	}
+
+	data, err := os.ReadFile(pngFilename)
 	if err != nil {
-		log.Fatal(err)
-	}
-	bounds := m.Bounds()
-	fmt.Println(bounds, formatString)
-
-	//Encode from image format to writer
-	timestamp := time.Now().Format("20060102150405")
-	pngFilename := "public/image/img_" + timestamp + ".png"
-
-	osFile, errOnOpenFIle := os.OpenFile(pngFilename, os.O_WRONLY|os.O_CREATE, 0777)
-	if errOnOpenFIle != nil {
-		log.Fatal(errOnOpenFIle)
+		return "", err
 	}
 
-	err = png.Encode(osFile, m)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	buffer := new(bytes.Buffer)
-	errWhileEncoding := png.Encode(buffer, m) // img is your image.Image
-	if errWhileEncoding != nil {
-		log.Fatal(errWhileEncoding)
-	}
-	srcImg := fmt.Sprintf("data:image/png;base64,%s", base64.StdEncoding.EncodeToString(buffer.Bytes()))
-	log.Info("Png file", pngFilename, "created")
+	srcImg := "data:image/png;base64," + base64.StdEncoding.EncodeToString(data)
+	log.Info("Reusing exist PNG file name: ", pngFilename, "as the output")
 
 	return srcImg, nil
 
@@ -74,37 +85,49 @@ func (f *File) Base64toJpg() (string, error) {
 	}
 
 	fileData := base64.StdEncoding.EncodeToString(f.FileData)
+	hasher := sha256.New()
+	hasher.Write([]byte(fileData))
+	hash := hex.EncodeToString(hasher.Sum(nil))
 
-	reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(fileData))
-	m, formatString, err := image.Decode(reader)
+	jpgFilename := "public/image/" + hash + ".jpg"
+
+	if _, err := os.Stat(jpgFilename); os.IsNotExist(err) {
+		reader := base64.NewDecoder(base64.StdEncoding, strings.NewReader(fileData))
+		m, formatString, err := image.Decode(reader)
+		if err != nil {
+			log.Fatal(err)
+		}
+		bounds := m.Bounds()
+		fmt.Println("base64toJpg", bounds, formatString)
+
+		osFile, err := os.OpenFile(jpgFilename, os.O_WRONLY|os.O_CREATE, 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = jpeg.Encode(osFile, m, &jpeg.Options{Quality: 75})
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		buffer := new(bytes.Buffer)
+		errWhileEncoding := jpeg.Encode(buffer, m, nil) // img is your image.Image
+		if errWhileEncoding != nil {
+			log.Fatal(errWhileEncoding)
+		}
+		srcImg := fmt.Sprintf("data:image/jpeg;base64,%s", base64.StdEncoding.EncodeToString(buffer.Bytes()))
+		log.Info("Create new JPG file name: ", jpgFilename, "as the output")
+
+		return srcImg, nil
+	}
+
+	data, err := os.ReadFile(jpgFilename)
 	if err != nil {
-		log.Fatal(err)
-	}
-	bounds := m.Bounds()
-	fmt.Println("base64toJpg", bounds, formatString)
-
-	//Encode from image format to writer
-	timestamp := time.Now().Format("20060102150405")
-	jpgFilename := "public/image/img_" + timestamp + ".jpg"
-
-	osFile, err := os.OpenFile(jpgFilename, os.O_WRONLY|os.O_CREATE, 0777)
-	if err != nil {
-		log.Fatal(err)
+		return "", err
 	}
 
-	err = jpeg.Encode(osFile, m, &jpeg.Options{Quality: 75})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	buffer := new(bytes.Buffer)
-	errWhileEncoding := jpeg.Encode(buffer, m, nil) // img is your image.Image
-	if errWhileEncoding != nil {
-		log.Fatal(errWhileEncoding)
-	}
-	srcImg := fmt.Sprintf("data:image/jpeg;base64,%s", base64.StdEncoding.EncodeToString(buffer.Bytes()))
-	log.Info("Png file", jpgFilename, "created")
+	srcImg := "data:image/jpeg;base64," + base64.StdEncoding.EncodeToString(data)
+	log.Info("Reusing exist JPG file name: ", jpgFilename, "as the output")
 
 	return srcImg, nil
-
 }
