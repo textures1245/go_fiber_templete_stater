359d004d6e04d13ec76e83f9c38b6922c80bf840
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-09 15:56:59 +0700

    update(file-usecase): update file-usecase package to handle file uploads and retrieval

--- internal/file/usecase/usecase.go
+++ internal/file/usecase/usecase.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"net/http"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/textures1245/go-template/internal/file"
 	"github.com/textures1245/go-template/internal/file/dtos"
 	"github.com/textures1245/go-template/internal/file/entities"
@@ -21,17 +22,66 @@ func NewFileUsecase(fileRepo file.FileRepository) file.FileUsecase {
 	}
 }
 
-func (u *fileUsecase) OnUploadFile(ctx context.Context, req *entities.FileUploaderReq) (int, *apperror.CErr) {
-	err := u.fileRepo.CreateFile(ctx, req)
-	if err != nil {
-		status, cErr := apperror.CustomSqlExecuteHandler("File", err)
-		return status, cErr
+func (u *fileUsecase) OnUploadFile(c *fiber.Ctx, ctx context.Context, req *entities.FileUploaderReq) (*dtos.FileSourceDataRes, int, *apperror.CErr) {
+	fileId, errCreateFile := u.fileRepo.CreateFile(ctx, req)
+
+	if errCreateFile != nil {
+		status, cErr := apperror.CustomSqlExecuteHandler("File", errCreateFile)
+		return nil, status, cErr
 	}
 
-	return http.StatusOK, nil
+	file, err := u.fileRepo.GetFileById(ctx, fileId)
+	if err != nil {
+		status, cErr := apperror.CustomSqlExecuteHandler("File", err)
+		return nil, status, cErr
+	}
+
+	var (
+		// base64urlRes string
+		fPathDatRes string
+	)
+	switch file.FileType {
+	case "PNG":
+		_, fPathDat, err := file.Base64toPng(c)
+		if err != nil {
+			status, cErr := apperror.CustomSqlExecuteHandler("File", err)
+			return nil, status, cErr
+		}
+		// base64urlRes = *base64url
+		fPathDatRes = *fPathDat
+	case "JPG":
+		_, fPathDat, err := file.Base64toJpg(c)
+		if err != nil {
+			status, cErr := apperror.CustomSqlExecuteHandler("File", err)
+			return nil, status, cErr
+		}
+		// base64urlRes = *base64url
+		fPathDatRes = *fPathDat
+	case "PDF":
+		_, fPathDat, err := file.Base64toFile(c, false)
+		if err != nil {
+			status, cErr := apperror.CustomSqlExecuteHandler("File", err)
+			return nil, status, cErr
+		}
+		// base64urlRes = *base64url
+		fPathDatRes = *fPathDat
+	default:
+		return nil, http.StatusBadRequest, apperror.NewCErr(errors.New("Only except for PNG and JPG for now"), errors.ErrUnsupported)
+	}
+
+	filesRes := &dtos.FileSourceDataRes{
+		FileName: file.FileName,
+		// FileBase64URL: base64urlRes,
+		FilePathData: fPathDatRes,
+		FileType:     file.FileType,
+		CreatedAt:    file.CreatedAt,
+		UpdatedAt:    file.UpdatedAt,
+	}
+
+	return filesRes, http.StatusOK, nil
 }
 
-func (u *fileUsecase) GetSourceFiles(ctx context.Context) ([]*dtos.FileSourceDataRes, int, *apperror.CErr) {
+func (u *fileUsecase) GetSourceFiles(c *fiber.Ctx, ctx context.Context) ([]*dtos.FileSourceDataRes, int, *apperror.CErr) {
 	files, err := u.fileRepo.GetFiles(ctx)
 	if err != nil {
 		status, cErr := apperror.CustomSqlExecuteHandler("File", err)
@@ -40,39 +90,46 @@ func (u *fileUsecase) GetSourceFiles(ctx context.Context) ([]*dtos.FileSourceDat
 
 	filesRes := []*dtos.FileSourceDataRes{}
 	for _, file := range files {
-		var srcFile string
+		var (
+			// base64urlRes string
+			fPathDatRes string
+		)
 		switch file.FileType {
 		case "PNG":
-			src, err := file.Base64toPng()
+			_, fPathDat, err := file.Base64toPng(c)
 			if err != nil {
 				status, cErr := apperror.CustomSqlExecuteHandler("File", err)
 				return nil, status, cErr
 			}
-			srcFile = src
+			// base64urlRes = *base64url
+			fPathDatRes = *fPathDat
 		case "JPG":
-			src, err := file.Base64toJpg()
+			_, fPathDat, err := file.Base64toJpg(c)
 			if err != nil {
 				status, cErr := apperror.CustomSqlExecuteHandler("File", err)
 				return nil, status, cErr
 			}
-			srcFile = src
+			// base64urlRes = *base64url
+			fPathDatRes = *fPathDat
 		case "PDF":
-			src, err := file.Base64toFile()
+			_, fPathDat, err := file.Base64toFile(c, true)
 			if err != nil {
 				status, cErr := apperror.CustomSqlExecuteHandler("File", err)
 				return nil, status, cErr
 			}
-			srcFile = src
+			// base64urlRes = *base64url
+			fPathDatRes = *fPathDat
 		default:
 			return nil, http.StatusBadRequest, apperror.NewCErr(errors.New("Only except for PNG and JPG for now"), errors.ErrUnsupported)
 		}
 
 		filesRes = append(filesRes, &dtos.FileSourceDataRes{
-			FileName:   file.FileName,
-			FileSrcURL: srcFile,
-			FileType:   file.FileType,
-			CreatedAt:  file.CreatedAt,
-			UpdatedAt:  file.UpdatedAt,
+			FileName: file.FileName,
+			// FileBase64URL: base64urlRes,
+			FilePathData: fPathDatRes,
+			FileType:     file.FileType,
+			CreatedAt:    file.CreatedAt,
+			UpdatedAt:    file.UpdatedAt,
 		})
 	}
 
