67d1242547b8c676e7e8778f47fa1fb1a3ddb800
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-09 17:02:25 +0700

    refactor(product-repo-query):  refactor product creation API to include validation and error handling

--- internal/product/entities/product_created.go
+++ internal/product/entities/product_created.go
@@ -3,10 +3,10 @@ package entities
 import "github.com/textures1245/go-template/internal/file/entities"
 
 type ProductCreatedReq struct {
-	Name        string                    `json:"product_name" db:"product_name" form:"product_name" binding:"required"`
-	Description string                    `json:"product_description" db:"product_description"  form:"product_description" binding:"required"`
-	Price       float64                   `json:"product_price"  db:"product_price" form:"product_price" binding:"required"`
-	Stock       int64                     `json:"stock_qty" db:"stock_qty"  form:"stock_qty" binding:"required"`
+	Name        string                    `json:"product_name" db:"product_name" form:"product_name" validate:"required" binding:"required"`
+	Description string                    `json:"product_description" db:"product_description"  form:"product_description"`
+	Price       float64                   `json:"price"  db:"product_price" form:"price" binding:"required" validate:"required,numeric"`
+	Stock       int64                     `json:"stock_qty" db:"stock_qty"  form:"stock_qty" binding:"required" validate:"required,number"`
 	File        *entities.FileUploaderReq `json:"file" form:"file" `
 }
 
@@ -14,7 +14,7 @@ type ProductCreatedReq struct {
 type ProductCreated struct {
 	Name        string  `json:"product_name" form:"product_name" db:"product_name" binding:"required"`
 	Description string  `json:"product_description" form:"product_description" db:"product_description" binding:"required"`
-	Price       float64 `json:"product_price" form:"product_price" db:"product_price" binding:"required"`
+	Price       float64 `json:"price" form:"price" db:"product_price" binding:"required"`
 	Stock       int64   `json:"stock_qty" form:"stock_qty" db:"stock_qty" binding:"required"`
 	FileId      *int64  `json:"file_id" db:"file_id"`
 }
---
+++ internal/product/repository/repository_query/product/create_product.sql
@@ -0,0 +1,10 @@
+INSERT INTO
+    Product (
+        product_name,
+        product_description,
+        product_price,
+        stock_qty,
+        file_fk_id
+    )
+VALUES
+    (?, ?, ?, ?, ?);
