1fc09699a27d359fa4f88dd178915a86f87fd474
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-05-10 10:41:21 +0700

    update(product-model): Add GetProducts method to ProductRepository interface and OnGetProducts method to ProductUsecase interface

--- internal/product/repository.go
+++ internal/product/repository.go
@@ -3,9 +3,11 @@ package product
 import (
 	"context"
 
+	"github.com/textures1245/go-template/internal/product/dtos"
 	"github.com/textures1245/go-template/internal/product/entities"
 )
 
 type ProductRepository interface {
 	CreateProduct(ctx context.Context, req *entities.ProductCreated) (*int64, error)
+	GetProducts(ctx context.Context) ([]*dtos.ProductDataRes, error)
 }
--- internal/product/usecase.go
+++ internal/product/usecase.go
@@ -3,11 +3,15 @@ package product
 import (
 	"context"
 
+	"github.com/gofiber/fiber/v2"
 	fileEntities "github.com/textures1245/go-template/internal/file/entities"
+	"github.com/textures1245/go-template/internal/product/dtos"
 	"github.com/textures1245/go-template/internal/product/entities"
 	"github.com/textures1245/go-template/pkg/apperror"
 )
 
 type ProductUsecase interface {
 	OnCreateProduct(ctx context.Context, prodReq *entities.ProductCreated, fileReq *fileEntities.FileUploaderReq) (int, *apperror.CErr)
+	OnGetProducts(c *fiber.Ctx, ctx context.Context) ([]*dtos.ProductDataRes, int, *apperror.CErr)
+	ExportDataAsExcel(c *fiber.Ctx, ctx context.Context) (*dtos.ProductToExcelRes, int, *apperror.CErr)
 }
